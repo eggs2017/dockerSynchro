4c5079d8e04aa75e1669fd02245996ebef1505be
eggs2017 <32611063+eggs2017@users.noreply.github.com>
2022-10-16 16:40:06 +0200

    Clean up.

--- client_alpine/entrypoint.sh
+++ client_alpine/entrypoint.sh
@@ -3,17 +3,6 @@
 # Start the run once job.
 echo "Docker container has been started - CRON service"
 
-#declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /container.env
-
-# Setup a cron schedule --setup this !!
-#echo "SHELL=/bin/bash
-#BASH_ENV=/container.env
-#@hourly /synchDir.sh '/mnt/c/MK'     >> /var/log/cron.log 2>&1
-#@daily  /synchDir.sh '/mnt/c/Ultra'  >> /var/log/cron.log 2>&1
-#This extra line makes it a valid cron ORG" > scheduler.txt
-
 crontab scheduler_rsync.txt
 #cron -f
 /usr/sbin/crond -f -l 8
-# Execute the CMD from the Dockerfile:
-#exec "$@"
