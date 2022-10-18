f6068af030a7cdc729e22a0a8c70e0dd2dd3dc22
eggs2017 <marekskoczy@gmail.com>
2022-10-18 16:55:39 +0200

    Root dir issue.

--- client_alpine/synchDir.sh
+++ client_alpine/synchDir.sh
@@ -7,7 +7,7 @@ ts=$(date +%d-%m-%Y_%H-%M-%S)
 echo "Synch Begin $ts" >> '/var/log/synch.log'
 echo "synchronization of $synchDir" >> '/var/log/synch.log'
 #exec
-rsync -arv -e "sshpass -f ssh_user.pass ssh -o StrictHostKeyChecking=no -p $server_port" \
+rsync -arv -e "sshpass -f /ssh_user.pass ssh -o StrictHostKeyChecking=no -p $server_port" \
     --progress "$synchDir" $ssh_user@$server_host:"$remoteDestDir"
 
 timeStstamp=$(date +%d-%m-%Y_%H-%M-%S)
